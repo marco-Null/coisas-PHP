dbb127abce9c57efc333afe25f4e62e32890f5f1
M_Green <marcopera990@gmail.com>
2026-05-26 13:13:06 -0300

    Update index.php

--- testePHP/index.php
+++ testePHP/index.php
@@ -1,48 +1,21 @@
-<?php
-    session_start();
-
-    
-$emailCerto = "Marco001@gmail.com";
-$senhaCerta = "67";
-
-    if ($_POST['email'] == $emailCerto) && ($_POST['senha'] == $senhaCerta) {
-
-    }
-
-
-    if(isset($_POST['email']) == $emailCerto && isset($_POST['senha']) == $senhaCerta){
-
-        $_SESSION['login'] = true;
-        $_SESSION['senha'] = true;
-        header('location: result.php');
-
-    }
-
-    else {
-        $_SESSION['login'] = false;
-        $_SESSION['senha'] = false;
-
-    }
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
+    <title>cadastro PHP</title>
 </head>
 <body>
-    <form method="post">
-        <label for="">cadastro </label>
-        <input type="email" name="email"><br><br>
+    <form action="metodo.php" method="post">
 
-        <label for="">senha</label>
-        <input type="password" name="senha"><br><Br><br>
+        <label for=""> Email: </label>
+        <input type="email" name="EmailCadas"><br><br>
+
+        <label for="">senha: </label>
+        <input type="password" name="SenhaCadas"><br><br>
 
         <input type="submit" value="Cadastrar">
+
     </form>
 </body>
 </html>
